aa7c4c3e3bc8be99d9408004401a25f01b508c02
Inny <inny101810@naver.com>
2024-04-19 23:37:37 +0900

    hardest one...

--- Week 1/Calculate_Factorial.py
+++ Week 1/Calculate_Factorial.py
@@ -1,1 +1,14 @@
+# for문 활용
+n = int(input("n!의 n을 입력하세요: "))
+factorial = 1
+for i in range(1, n+1, 1):
+    factorial = factorial * i
+print(f"{n}! =", factorial)
 
+# while문 활용
+n = int(input("n!의 n을 입력하세요: "))
+factorial = 1
+while n >= 1:
+    factorial = factorial * n
+    n = n - 1
+print(factorial)
--- Week 1/Prime_Numbers.py
+++ Week 1/Prime_Numbers.py
@@ -1,1 +1,15 @@
-
+print("소수판별기")
+pn = int(input("숫자를 입력하세요: "))
+a = 0
+if pn < 2:
+    print(f"""{pn}은/는 소수가 아닙니다. 소수는 2보다 크거나 같아야 합니다.
+프로그램을 다시 실행해주세요.""")
+else:
+    for i in range(2, pn):
+        result = pn % i
+        if result == 0:
+            a = a + 1
+    if a == 0:
+        print(f"{pn}은(는) 소수입니다.")
+    else:
+        print(f"{pn}은(는) 소수가 아닙니다.")
